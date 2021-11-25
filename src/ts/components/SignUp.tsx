804940086fe430ef579e4bbe0055bb9b5c6fcaf9
Troy DeSpain <despaintroy@gmail.com>
2021-11-24 23:25:59 -0700

    Validation on signup

--- src/ts/components/SignUp.tsx
+++ src/ts/components/SignUp.tsx
@@ -1,7 +1,15 @@
 import * as React from 'react'
 
 import { getMessage } from 'ts/services/errors'
+import { FormState } from 'ts/services/models'
 import { signUp } from 'ts/services/user'
+import { EMAIL_REGEX } from 'ts/utils/constants'
+import {
+	beforeSubmit,
+	handleValueChange,
+	newFormState,
+	validateForm,
+} from 'ts/utils/helpers'
 import Paths from 'ts/utils/paths'
 
 import PersonIcon from '@mui/icons-material/Person'
@@ -10,37 +18,72 @@ import Avatar from '@mui/material/Avatar'
 import Box from '@mui/material/Box'
 import Button from '@mui/material/Button'
 import Container from '@mui/material/Container'
-import Grid from '@mui/material/Grid'
 import TextField from '@mui/material/TextField'
 import Typography from '@mui/material/Typography'
 
+const fieldsArray = ['name', 'email', 'password1', 'password2'] as const
+type Fields = typeof fieldsArray[number]
+
+const nameValidator = (state: FormState<Fields>): FormState<Fields> => {
+	state.isValid.name = state.values.name.length > 0
+	state.messages.name = state.isValid.name ? '' : 'Required'
+	return { ...state }
+}
+
+const emailValidator = (state: FormState<Fields>): FormState<Fields> => {
+	state.isValid.email = !!state.values.email.match(EMAIL_REGEX)
+	state.messages.email = state.isValid.email ? '' : 'Invalid email address'
+	return { ...state }
+}
+
+const password1Validator = (state: FormState<Fields>): FormState<Fields> => {
+	state.isValid.password1 = state.values.password1.length > 4
+	state.messages.password1 = state.isValid.password1
+		? ''
+		: 'Must be at least 5 characters long'
+	return { ...state }
+}
+
+const password2Validator = (state: FormState<Fields>): FormState<Fields> => {
+	state.isValid.password2 = state.values.password2 === state.values.password1
+	state.messages.password2 = state.isValid.password2
+		? ''
+		: 'Passwords do not match'
+	return { ...state }
+}
+
 export default function SignUp(): React.ReactElement {
-	const [errorText, setErrorText] = React.useState('')
 	const [isLoading, setIsLoading] = React.useState(false)
 
-	const handleSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
+	const [formState, setFormState] = React.useState<FormState<Fields>>({
+		...newFormState(fieldsArray),
+		validators: {
+			name: nameValidator,
+			email: emailValidator,
+			password1: password1Validator,
+			password2: password2Validator,
+		},
+	})
+
+	function handleSubmit(event: React.FormEvent<HTMLFormElement>): void {
 		event.preventDefault()
-		setErrorText('')
 
-		const data = new FormData(event.currentTarget)
-		const name = data.get('name')?.toString()
-		const email = data.get('email')?.toString()
-		const password1 = data.get('password1')?.toString()
-		const password2 = data.get('password2')?.toString()
+		setFormState(state => beforeSubmit(state))
 
-		if (!email || !password1 || !password2 || !name) {
-			setErrorText('Please enter your name, email, and password.')
-			return
-		}
-
-		if (password1 !== password2) {
-			setErrorText('Passwords do not match.')
-			return
-		}
+		if (!formState.formValid) return
 
 		setIsLoading(true)
-		signUp(email, password1, name)
-			.catch(e => setErrorText(getMessage(e)))
+
+		signUp(
+			formState.values.email,
+			formState.values.password1,
+			formState.values.name
+		)
+			.catch(e =>
+				setFormState(state => {
+					return { ...state, formMessage: getMessage(e) }
+				})
+			)
 			.finally(() => setIsLoading(false))
 	}
 
@@ -70,6 +113,15 @@ export default function SignUp(): React.ReactElement {
 						name='name'
 						autoComplete='name'
 						autoFocus
+						onChange={(e): void =>
+							setFormState(state => handleValueChange(e, state))
+						}
+						onBlur={(): void => {
+							formState.touched.name = true
+							setFormState(state => validateForm(state))
+						}}
+						error={formState.touched.name && !formState.isValid.name}
+						helperText={formState.touched.name && formState.messages.name}
 					/>
 					<TextField
 						margin='normal'
@@ -79,6 +131,15 @@ export default function SignUp(): React.ReactElement {
 						label='Email Address'
 						name='email'
 						autoComplete='email'
+						onChange={(e): void =>
+							setFormState(state => handleValueChange(e, state))
+						}
+						onBlur={(): void => {
+							formState.touched.email = true
+							setFormState(state => validateForm(state))
+						}}
+						error={formState.touched.email && !formState.isValid.email}
+						helperText={formState.touched.email && formState.messages.email}
 					/>
 					<TextField
 						margin='normal'
@@ -89,6 +150,17 @@ export default function SignUp(): React.ReactElement {
 						type='password'
 						id='password1'
 						autoComplete='new-password'
+						onChange={(e): void =>
+							setFormState(state => handleValueChange(e, state))
+						}
+						onBlur={(): void => {
+							formState.touched.password1 = true
+							setFormState(state => validateForm(state))
+						}}
+						error={formState.touched.password1 && !formState.isValid.password1}
+						helperText={
+							formState.touched.password1 && formState.messages.password1
+						}
 					/>
 					<TextField
 						margin='normal'
@@ -99,10 +171,21 @@ export default function SignUp(): React.ReactElement {
 						type='password'
 						id='password2'
 						autoComplete='new-password'
+						onChange={(e): void =>
+							setFormState(state => handleValueChange(e, state))
+						}
+						onBlur={(): void => {
+							formState.touched.password2 = true
+							setFormState(state => validateForm(state))
+						}}
+						error={formState.touched.password2 && !formState.isValid.password2}
+						helperText={
+							formState.touched.password2 && formState.messages.password2
+						}
 					/>
-					{errorText && (
+					{formState.attemptedSubmit && formState.formMessage && (
 						<Alert sx={{ mt: 2 }} severity='error'>
-							{errorText}
+							{formState.formMessage || 'Form error'}
 						</Alert>
 					)}
 					<Button
@@ -117,18 +200,9 @@ export default function SignUp(): React.ReactElement {
 							'Sign Up'
 						)}
 					</Button>
-					<Grid container>
-						<Grid item xs>
-							<Link href={Paths.signIn} variant='body2'>
-								{'Already have an account? Sign In'}
-							</Link>
-						</Grid>
-						<Grid item>
-							{/* <Link href='#' variant='body2'>
-								Forgot password?
-							</Link> */}
-						</Grid>
-					</Grid>
+					<Link href={Paths.signIn} variant='body2'>
+						{'Already have an account? Sign In'}
+					</Link>
 				</Box>
 			</Box>
 		</Container>
